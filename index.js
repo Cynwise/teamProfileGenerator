d2014c016e9444b484718fd4582ae47b0ccf2e2b
William Farnsworth <williamcoreyf@gmail.com>
2021-06-09 18:32:05 -0400

    Implemented input manager inquirer function

--- index.js
+++ index.js
@@ -5,3 +5,39 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+function inputManager() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter manager name:',
+            name: 'managerName',
+        },
+        {
+            type: 'input',
+            message: 'Enter ID number:',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Enter manager email address:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Enter manager office phone number:',
+            name: 'officeNumber',
+        }
+    ])
+    .then(function (data) {
+
+        const name = data.managerName;
+        const id = data.id;
+        const email = data.email;
+        const officeNumber = data.officeNumber;
+        const teamManager = new Manager(name, id, email, officeNumber);
+        console.log(teamManager);
+    })
+}
+
+inputManager();

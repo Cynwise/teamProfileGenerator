7f251340d56fa9c2be3db5a228d2153812fc30c5
William Farnsworth <williamcoreyf@gmail.com>
2021-06-11 15:22:25 -0400

    implemented the generateHtml function

--- index.js
+++ index.js
@@ -125,7 +125,68 @@ function inputTeam() {
 
             console.log('compiling team information:');
             console.log(projectTeam);
+            generateHtml();
         }
 
+    })
+}
+
+function generateHtml() {
+    const htmlArray = []
+    const initHtml = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <title>Document</title>
+    </head>
+    <body>
+    <header>
+        <h2>Project Team</h2>
+    </header>
+    <div class="container row-12"> `
+    htmlArray.push(initHtml);
+
+
+    for (let i = 0; i < projectTeam.length; i++) {
+        let newCard = `
+        <div class="card column-4" style="width: 18rem;">
+        <div class="card-body">
+        <div class="card-header">
+            
+            <h3 id="larger">${projectTeam[i].name}</h3>
+        </div>
+        <div class="card-text">
+            <p>Id: ${projectTeam[i].id}</p>
+            <p>Email: <a href="mailto:${projectTeam[i].email}" ?subject=subject text">${projectTeam[i].email}</a></p>`
+        if (projectTeam[i].officeNumber) {
+            newCard += `
+                <p>Office Number: ${projectTeam[i].officeNumber}</p>`
+        } else if (projectTeam[i].github) {
+            newCard += `
+            <p>Github: <a href="https://github.com/${projectTeam[i].github}" target="_blank">${projectTeam[i].github}</a></p>`
+        } else if (projectTeam[i].school) {
+            newCard += `
+            <p>School: ${projectTeam[i].school}</p>`
+        }
+        newCard += `</div>
+        </div>`
+        htmlArray.push(newCard)
+    }
+
+    const secondHtml = `
+    </div>
+    </body>
+    
+    </html`
+    htmlArray.push(secondHtml);
+
+
+
+    fs.writeFile('index.html', htmlArray.join(""), (err) => {
+        if (err) throw err;
     })
 }
